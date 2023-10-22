3ee76439724921c4ba34e1b04fc20300d12c4368
MACOS <huynhnhinguyen0301@gmail.com>
2023-10-22 14:55:58 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VideoModule } from './video/video.module';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
+  imports: [VideoModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AppService {
+  prisma = new PrismaClient();
+
   getHello(): string {
+
+
+
     return 'Hello World!';
   }
 }
